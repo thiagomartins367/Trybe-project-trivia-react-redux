ffb526f9cf3bf77c389bb907a5e49e8aa69494ef
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-02-18 19:09:34 -0300

    Requisito 15 feito.

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
@@ -8,12 +9,17 @@ class Feedback extends React.Component {
     const { assertionsRedux } = this.props;
     const COMPARE_SCORE = 3;
     return (
-      <div>
+      <section>
         <Header />
         <p data-testid="feedback-text">
-          {assertionsRedux >= COMPARE_SCORE ? 'Well Done!' : 'Could be better...'}
+          { assertionsRedux >= COMPARE_SCORE ? 'Well Done!' : 'Could be better...' }
         </p>
-      </div>
+        <Link to="/">
+          <button type="button" data-testid="btn-play-again">
+            Play Again
+          </button>
+        </Link>
+      </section>
     );
   }
 }
